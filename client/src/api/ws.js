7a0d29405a10a603d309db45edddc5b8613f1ea9
Kseniia98 <kseniia.plakhotnia@gmail.com>
2022-07-05 16:35:05 +0300

    Fix ws connection

---
+++ client/src/api/ws.js
@@ -0,0 +1,22 @@
+import {io} from 'socket.io-client';
+
+const socket = io('ws://localhost:5000', {
+  auth: {userId: '123'},
+  transports: ["websocket"]
+});
+
+socket.on('hello', (data, param) => {
+  console.log('server said hello', data, param);
+  socket.emit('hello', "from client")
+});
+
+socket.on('newMessage', (newMsg) => {
+  const sub = subscribers.find(sub => sub.eventName === 'newMessage')
+  sub.callback(newMsg)
+});
+
+const subscribers = [];
+
+export const addSubscriber = (eventName, callback) => {
+  subscribers.push(eventName, callback)
+}
--- server/src/ws.js
+++ server/src/ws.js
@@ -1,15 +1,23 @@
 const { Server } = require("socket.io");
 
+const eventTypes = {
+  HELLO: 'hello'
+}
+
 module.exports = (httpServer) => {
   const io = new Server(httpServer, {
     //options
+    cors: {
+      origin: ['http://localhost:3000/']
+    }
   });
 
   io.on('connection', (socket) => {
     console.log('some user has connected');
+    socket.emit(eventTypes.HELLO, { data: 'test'})
   
-    socket.on('hello', () => {
-      console.log('someone said Hello');
+    socket.on(eventTypes.HELLO, (data, param) => {
+      console.log('someone said Hello', data, param);
     })
   })
 }
